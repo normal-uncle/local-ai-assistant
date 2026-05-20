9c1f244f10f16bc9601167ed680907278d14fddd
chlollo <fianl@twave.co.kr>
2026-05-20 11:26:06 +0900

    chore: Gradle 멀티 모듈 루트 + version catalog + ktlint 셋업

---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Assistant"
+include(":app")
+include(":ui_component")
+include(":local")
+include(":repository")
+include(":usecase")
+include(":feature_capture")
+include(":feature_memo")
